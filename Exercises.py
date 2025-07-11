3c55c41d3d639362b78b3f1db8213e5f57fd2f7a
Karandeep-56 <karandeep.singh9121@gmail.com>
2025-07-10 22:26:27 -0700

    creating list

---
+++ Exercises.py
@@ -0,0 +1,15 @@
+#creating list
+
+fruits = ["apple","banana", "carrot", "mango", "grapes"]
+print("initial list", fruits)
+
+#updatng a list
+fruits.append("orange")
+print("list after adding orange", fruits)
+
+#accessing list elements
+print('first fruit', fruits[0])
+print("last fruit", fruits[-1])
+print("second and third fruit",fruits[1:3])
+
+
